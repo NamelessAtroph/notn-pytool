66e12caef3d6a8948e9b9850aaf12dc7dbdb3400
Vhou <josephtlavin01@gmail.com>
2020-08-19 21:42:42 +0000

    stats window

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from tkinter import *
-import tkinter.messagebox as box
 import pickle,os
 
 #Base
 global window
 global year
 window=Tk()
+window.title("NotN Pytool")
+window.resizable(0,0)
 
 #col1
 col1=Frame(window)
@@ -46,6 +47,10 @@ crTxt=Label(otherFrame,text="Crossroads:")
 crCount=Entry(otherFrame)
 crUp=Button(otherFrame,text="+")
 crCount.insert(0,'0')
+gthTxt=Label(otherFrame,text="Gathering:")
+gthCount=Entry(otherFrame)
+gthUp=Button(otherFrame,text="+")
+gthCount.insert(0,'0')
 #dataFrame
 dataFrame=Frame(col2)
 dataLabel=Label(dataFrame,text="Stored Data:",font='BOLD')
@@ -53,6 +58,7 @@ dataInfo=Text(dataFrame,width=25,height=10,state=DISABLED)
 #databtns
 btnFrame=Frame(col2)
 writfil=Button(btnFrame,text="Write File")
+statss=Button(btnFrame,text="Stats")
 
 #col1 Geometry
 col1.grid(column=0,row=0)
@@ -87,17 +93,22 @@ ahUp.grid(row=3,column=2,padx=5,pady=2)
 crTxt.grid(row=4,column=0)
 crCount.grid(row=4,column=1)
 crUp.grid(row=4,column=2,padx=5,pady=2)
+gthTxt.grid(row=5,column=0)
+gthCount.grid(row=5,column=1)
+gthUp.grid(row=5,column=2,padx=5,pady=2)
 #dataFrame Geometry
 dataFrame.grid(column=1,padx=8)
 dataLabel.grid(row=1)
 dataInfo.grid()
 #btnFrame Geometry
 btnFrame.grid(column=1,pady=5)
-writfil.grid(row=2)
+writfil.grid(row=2,pady=3)
+statss.grid(row=3,pady=3)
 
 #Functions
 class guiFuncts:
 
+	#File tasks
 	def openFile():
 		yr=year.get()
 		file=open('docs/year/'+yr+'.dat','ab+')
@@ -122,6 +133,8 @@ class guiFuncts:
 		ahCount.insert(0,dat['ahchests'])
 		crCount.delete(0,'end')
 		crCount.insert(0,dat['crosschests'])
+		gthCount.delete(0,'end')
+		gthCount.insert(0,dat['gthchests'])
 	def writeFile():
 		c1=enCount.get()
 		c2=colChestCount.get()
@@ -129,7 +142,8 @@ class guiFuncts:
 		c4=swpCount.get()
 		c5=ahCount.get()
 		c6=crCount.get()
-		counts=dict={'enemies':c1,'colichests':c2,'baldchests':c3,'swipchests':c4,'ahchests':c5,'crosschests':c6}
+		c7=gthCount.get()
+		counts=dict={'enemies':c1,'colichests':c2,'baldchests':c3,'swipchests':c4,'ahchests':c5,'crosschests':c6,'gthchests':c7}
 		print(counts)
 		dataInfo.configure(state=NORMAL)
 		dataInfo.delete('1.1',END)
@@ -172,7 +186,46 @@ class guiFuncts:
 		crCount.delete(0,'end')
 		val2=int(val1)+1
 		crCount.insert(0,val2)
+	def gthUpf():
+		val1=gthCount.get()
+		gthCount.delete(0,'end')
+		val2=int(val1)+1
+		gthCount.insert(0,val2)
 
+	#OtherWindows
+	def stw():
+		#Base
+		st=Toplevel(window)
+		st.title("Chest Statistics")
+		#Enter year
+		yearFrame=Frame(st)
+		yrLabel=Label(yearFrame,text="Year: ")
+		syear=Entry(yearFrame)
+		yrBtn=Button(yearFrame,text="Open",width=4)
+		#Stats
+		stats=Frame(st)
+		yscroll=Scrollbar(stats)
+		stwindow=Text(stats,width=25,height=10,state=DISABLED,yscrollcommand=yscroll.set)
+		yscroll.config(command=stwindow.yview)
+		#Geometry
+		yearFrame.grid(row=0,column=0)
+		yrLabel.grid(row=0,column=0)
+		syear.grid(row=0,column=1)
+		yrBtn.grid(row=0,column=2,padx=5)
+		stats.grid(row=1,column=0)
+		stwindow.grid(row=2,column=0)
+		#Do the stat thing
+		def runSt():
+			yr=syear.get()
+			file=open('docs/year/'+yr+'.dat','rb')
+			dat=pickle.load(file)
+			file.close()
+			stwindow.configure(state=NORMAL)
+			stwindow.insert("1.1","-Stats for: "+yr+"-\n"+"Coli enemies fought: "+dat['enemies']+"\nChests from Coli: "+dat['colichests']+"\nDrop rate: "+str(int(dat['colichests'])/int(dat['enemies'])*100)+"%\nBaldwin chests: "+dat['baldchests']+"\nSwipp chests: "+dat['swipchests']+"\nAH chests: "+dat['ahchests']+"\nCR chests:"+dat['crosschests']+"\nGathering chests: "+dat['gthchests'])
+			stwindow.configure(state=DISABLED)
+		#btn
+		yrBtn.configure(command=runSt)
+	
 #FileButtons
 yrBtn.configure(command=guiFuncts.openFile)
 #ColiButtons
@@ -183,8 +236,10 @@ bdwUp.configure(command=guiFuncts.bdwUpf)
 swpUp.configure(command=guiFuncts.swpUpf)
 ahUp.configure(command=guiFuncts.ahUpf)
 crUp.configure(command=guiFuncts.crUpf)
+gthUp.configure(command=guiFuncts.gthUpf)
 #dataBtns
 writfil.configure(command=guiFuncts.writeFile)
+statss.configure(command=guiFuncts.stw)
 
 #Run app
 window.mainloop()
